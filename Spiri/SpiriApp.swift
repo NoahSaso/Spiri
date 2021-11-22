2dcf9a0bbc2d91278653e952a2e4251721319589
Noah Saso <noahsaso@gmail.com>
2021-11-21 19:29:21 -0800

    Initial Commit

---
+++ Spiri/SpiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiriApp.swift
+//  Spiri
+//
+//  Created by Noah Saso on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct SpiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
